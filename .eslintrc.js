fe11d44e11c8965ac0a2f5689df8c3c9e6f39594
Daniil Ognevoy <roxxor503@gmail.com>
2022-06-09 12:55:14 +0300

    added project structure

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: { ecmaVersion: 8, sourceType: 'module' },
+  ignorePatterns: ['node_modules/*'],
+  extends: ['eslint:recommended', 'airbnb', 'prettier'],
+};
