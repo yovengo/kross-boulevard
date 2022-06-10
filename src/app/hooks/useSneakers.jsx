6cfa1d07239e398d1aa605d176b2bf2ea33952f6
Daniil Ognevoy <roxxor503@gmail.com>
2022-06-10 21:07:33 +0300

    added custom hooks for brand, materials & sneakers

---
+++ src/app/hooks/useSneakers.jsx
@@ -0,0 +1,51 @@
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import sneakersService from '../services/sneakers.service';
+
+const SneakersContext = React.createContext();
+
+export const useSneakers = () => {
+  return useContext(SneakersContext);
+};
+
+export const SneakersProvider = ({ children }) => {
+  const [sneakers, setSneakers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getSneakers();
+  }, []);
+
+  useEffect(() => {
+    if (error !== null) {
+      console.log(error);
+      setError(null);
+    }
+  }, [error]);
+
+  async function getSneakers() {
+    try {
+      const { context } = await sneakersService.get();
+      setSneakers(context);
+      setLoading(false);
+    } catch (error) {
+      errorCatcher(error);
+    }
+  }
+
+  function errorCatcher(error) {
+    const { message } = error.response.data;
+    setError(message);
+  }
+
+  return (
+    <SneakersContext.Provider value={sneakers}>
+      {!isLoading ? children : 'Loading...'}
+    </SneakersContext.Provider>
+  );
+};
+
+SneakersProvider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+};
