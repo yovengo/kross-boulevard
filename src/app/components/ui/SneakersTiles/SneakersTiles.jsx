e3107a061ae2ded472e4ecf2f20cc681a368265b
Daniil Ognevoy <roxxor503@gmail.com>
2022-06-14 00:19:30 +0300

    improved tiles & added logo

--- src/app/components/ui/SneakersTiles/SneakersTiles.jsx
+++ src/app/components/ui/SneakersTiles/SneakersTiles.jsx
@@ -10,20 +10,28 @@ const SneakersTiles = () => {
     <>
       {sneakers.map((s) => (
         <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={s._id}>
-          <Link to={`/sneakers/${s._id}`} className="block relative h-48 rounded overflow-hidden">
-            <img
-              alt="ecommerce"
-              className="object-cover object-center w-full h-full block"
-              src={s.image}
-            />
+          <Link to={`/sneakers/${s._id}`}>
+            <div className="border-2 bg-[#f3f1f4] rounded-xl pt-24">
+              <div className="block relative h-48 rounded overflow-hidden">
+                <img
+                  alt="ecommerce"
+                  className="object-cover object-center w-full h-full block"
+                  src={s.image}
+                />
+              </div>
+              <div className="pt-4 pl-4 pb-4">
+                <h3 className="text-gray-500 text-sm tracking-widest title-font mb-1 ">
+                  <span className="bg-gray-200 px-1 py-0.5">
+                    <Brand id={s.brand} />
+                  </span>
+                </h3>
+                <h2 className="text-gray-900 hover:text-red-700 transition-colors title-font lg:text-lg font-medium">
+                  {s.name}
+                </h2>
+                <p className="mt-1">{s.price} &#8381;</p>
+              </div>
+            </div>
           </Link>
-          <div className="mt-4">
-            <h3 className="text-gray-500 text-sm tracking-widest title-font mb-1">
-              <Brand id={s.brand} />
-            </h3>
-            <h2 className="text-gray-900 title-font text-lg font-medium">{s.name}</h2>
-            <p className="mt-1">{s.price} &#8381;</p>
-          </div>
         </div>
       ))}
     </>
