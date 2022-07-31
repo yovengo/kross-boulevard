a9e4321055a1844220935e3e890f0fe6b2529292
Daniil Ognevoy <roxxor503@gmail.com>
2022-07-31 10:01:23 +0300

    connected to MongoDB

--- server/app.js
+++ server/app.js
@@ -10,6 +10,17 @@ app.use(express.urlencoded({extended: false}))
 
 const PORT = config.get('port') ?? 8080
 
-app.listen(8080, () => {
-    console.log(chalk.green(`Server has been started on port ${PORT}...`))
-})
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'))
+        console.log(chalk.green('MongoDB connected.'))
+        app.listen(8080, () => {
+            console.log(chalk.green(`Server has been started on port ${PORT}...`))
+        })
+    } catch (error) {
+        console.log(chalk.red(error.message))
+        process.exit(1)
+    }
+}
+
+start()
