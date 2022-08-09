48f9706ad7d74fdc50f143bd102dcb7bdd776878
Daniil Ognevoy <roxxor503@gmail.com>
2022-08-09 16:34:50 +0300

    added signUp method & token service

--- server/models/User.js
+++ server/models/User.js
@@ -11,6 +11,7 @@ const schema = new Schema(
       required: true,
       unique: true,
     },
+    password: { type: String },
     cart: [{ type: String }],
   },
   {
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,8 +1,36 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const tokenService = require('../services/token.service');
 const router = express.Router({ mergeParams: true });
 
 router.post('/signUp', async (req, res) => {
   try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        error: {
+          message: 'EMAIL_EXISTS',
+          code: 400,
+        },
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = await User.create({
+      ...req.body,
+      password: hashedPassword,
+      cart: [],
+    });
+
+    const tokens = tokenService.generate({ _id: newUser._id });
+    await tokenService.save(newUser._id, tokens.refreshToken);
+
+    res.status(201).send({ ...tokens, userId: newUser._id });
   } catch (e) {
     res.status(500).json({
       message: 'An error has occurred on the server. Try later',
