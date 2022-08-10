c96c81ae623c910672c5a98c516dce834b8fa416
Daniil Ognevoy <roxxor503@gmail.com>
2022-08-10 13:08:01 +0300

    added user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,4 +1,34 @@
 const express = require('express');
+const auth = require('../middleware/auth.middleware');
+const User = require('../models/User');
 const router = express.Router({ mergeParams: true });
+
+router
+  .route('/cart')
+  .get(auth, async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.user._id);
+      const currentCart = currentUser?.cart;
+
+      res.status(200).send(currentCart);
+    } catch (e) {
+      res.status(500).json({ message: 'An error has occurred on the server. Try later' });
+    }
+  })
+  .patch(auth, async (req, res) => {
+    try {
+      const userId = req.user._id;
+
+      if (userId) {
+        const updatedUserCart = await User.findByIdAndUpdate(userId, req.body, { new: true });
+        res.send(updatedUserCart);
+      } else {
+        res.status(401).json({ message: 'Unauthorized' });
+      }
+    } catch (e) {
+      res.status(500).json({ message: 'An error has occurred on the server. Try later' });
+    }
+  })
+  .delete();
 
 module.exports = router;
