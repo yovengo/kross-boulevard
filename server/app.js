c02e884ec91ee17afa04807a893a82d327597b14
Daniil Ognevoy <roxxor503@gmail.com>
2022-09-01 13:31:20 +0300

    improved adaptive

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const chalk = require('chalk');
 const cors = require('cors');
+const path = require('path');
 const initDatabase = require('./startUp/initDatabase');
 const routes = require('./routes');
 
@@ -15,6 +16,16 @@ app.use(cors());
 app.use('/api', routes);
 
 const PORT = config.get('port') ?? 8080;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client')));
+
+  const indexPath = path.join(__dirname, 'client', 'index.html');
+
+  app.get('*', (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 async function start() {
   try {
